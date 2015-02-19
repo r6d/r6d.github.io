cb7f6f9a847b7ac9bf22113aaad12ecf5dc75df5
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2015-02-19 17:08:51 +0100

    Delete quassel_stats_by_network.sql

--- _posts/quassel_stats_by_network.sql
+++
@@ -1,8 +0,0 @@
-﻿SELECT count(*) as count__________, n.networkname, n.networkid
-FROM backlog
-NATURAL JOIN buffer buf
-NATURAL JOIN network n
-
-GROUP BY n.networkname, n.networkid
-ORDER BY count(*) DESC
-LIMIT 100;
